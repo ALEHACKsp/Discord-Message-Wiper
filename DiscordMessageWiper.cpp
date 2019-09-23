fd91bbab74d6d3bee2262a655a824c29c5faeff1
Ivan <55006191+IvaN4GameS@users.noreply.github.com>
2019-09-23 02:58:47 +0200

    Add files via upload

---
+++ DiscordMessageWiper.cpp
@@ -0,0 +1,46 @@
+// DiscordMessageWiper.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+
+
+int main()
+{
+
+	std::cout << "Discord Message Wiper by IvaN4GameS!\n";
+	std::cout << "\n";
+	std::cout << "Hold F4 to start deleting discord messages.\n";
+	
+
+	while (TRUE)
+	{
+
+        
+		Sleep(1);
+		if (GetKeyState(0x73) & 0x8000) {
+
+			keybd_event(VK_UP, 0, 0, 0);
+			keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_CONTROL, 0x9d, 0, 0); 
+			keybd_event(VkKeyScan(0x41), 0x9e, 0, 0);
+			keybd_event(VkKeyScan(0x41), 0x9e, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_CONTROL, 0x9d, KEYEVENTF_KEYUP, 0); 
+			keybd_event(VK_BACK, 0, 0, 0);
+			keybd_event(VK_BACK, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_RETURN, 0, 0, 0); 
+			keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_RETURN, 0, 0, 0);
+			keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+			Sleep(80);
+			
+			
+		
+
+			
+		}
+	}
+	
+}
+
